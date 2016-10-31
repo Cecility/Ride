441c049ef029061a4aaac5f128f490b1b7fdc0f2
Hansol You <h5you@ucsd.edu>
2016-10-31 01:48:56 -0700

    testing

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET login page. */
 router.get('/', function(req, res, next) {
 	res.render('index');
 });
 
-/* GET home page. */
+/* GET home / main / groups page. */
 router.get('/main', function(req, res, next) {
 	var fs = require('fs');
 	var data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 	console.log(data)
 	res.render('main', { userprofile: data });
+});
+
+/* GET discovery page. */
+router.get('/discovery', function(req, res, next) {
+	var fs = require('fs');
+	var data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+	console.log(data)
+	res.render('discovery', { userprofile: data });
+});
+
+
+/* GET settings page. */
+router.get('/settings', function(req, res, next) {
+	var fs = require('fs');
+	var data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+	console.log(data)
+	res.render('settinhs', { userprofile: data });
 });
 
 
