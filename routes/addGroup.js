c61e5d9e1d6f512b26d362083fe9ed6eaa3c0cb8
Hansol You <h5you@ucsd.edu>
2016-11-03 19:46:03 -0700

    Discovery page now loading things

--- routes/addGroup.js
+++ routes/addGroup.js
@@ -1,3 +1,10 @@
 exports.view = function(req, res, next) {
 	res.render('addGroup');
 };
+
+/*exports.addGroup = function(req, res) {
+		var location = {'pickupX': req.query.x, 'pickupY': req.query.y};
+
+	addGroup.drives.push(location);
+}
+*/
