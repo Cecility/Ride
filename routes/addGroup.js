0d7132ae2492c0a961ffcd8f2540ac35758c7e41
Hansol You <h5you@ucsd.edu>
2016-11-03 22:14:25 -0700

    nothing changed

--- routes/addGroup.js
+++ routes/addGroup.js
@@ -1,5 +1,8 @@
 exports.view = function(req, res, next) {
-	res.render('addGroup');
+	var fs = require('fs');
+	var data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+	console.log(data)
+	res.render('addGroup', { userprofile: data });
 };
 
 /*exports.addGroup = function(req, res) {
