6c876856e1eebe146c595fee608f6cde9f81605c
Macadmin <macadmin@dss-ant-c07pw08tg1j0.ucsd.edu>
2016-11-08 22:26:59 -0800

    reset

--- config/auth.js
+++
@@ -1,7 +0,0 @@
-module.export = {
-    'googleAuth' : {
-        'clientID':'344088152572-shpan01cdgurfgrmggcbid0ids6sf3h5.apps.googleusercontent.com',
-        'clientSecret':'gjcm2RxLwACfqwq5lpHB1d0z',
-        'callbackUrl': 'https://ride-175f3.firebaseapp.com/__/auth/handler'
-    }
-}
--- models/user.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var userSchema = mongoose.Schema({
-    google:{
-        id: String,
-        token: String,
-        email: Stirng,
-        name: String
-    }
-})
