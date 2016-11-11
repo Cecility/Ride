92a131b038101f5f5d61a7eb36e408457980012f
Michael Zhong <m2zhong@ucsd.edu>
2016-11-11 01:12:33 -0800

    finished adding groups to database

--- routes/discovery.js
+++ routes/discovery.js
@@ -24,12 +24,34 @@ exports.view = function(req, res, next) {
 
 exports.create = function (req, res){
     var groupId = req.body.groupId;
-    models.user.find({'_id': "58255be0d9675d0012998223"}).update({"group": groupId}).exec(afterUpdating);
+    var currGroupId = [];
+    
+    models.user.find({'_id':"582552e6dcba0f326cc71a6e"}, function(err, userData){
+        if (userData.length > 0)
+            currGroupId = userData[0].groups;
+        
+        else{
+            console.log("user does not exist on database");
+            console.log(err);
+        }
+    
+        
+        for(var i = 0; i < userData[0].groups.length; i++){
+            currGroupId[i] = userData[0].groups[i];
+            console.log("currently adding " + currGroupId[i]);
+        }
+        
+        currGroupId[currGroupId.length] = groupId;
+        console.log("to be uplaoded " + currGroupId);
+        
+         models.user.find({'_id': "582552e6dcba0f326cc71a6e"}).update({"groups": currGroupId}).exec(afterUpdating);
     
     function afterUpdating(err){
         if(err)
             console.log(err);
         else
-            console.log(groupId);
-    }
+            console.log("new id added was " + groupId);
+    }   
+    });
+
 };
