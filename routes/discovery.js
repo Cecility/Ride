0d1d313fe41789f8d64a614dd328a9ebfc2ecb4c
Hansol You <h5you@ucsd.edu>
2016-11-10 23:47:55 -0800

    god fuck this shit i connected online db to the actual pages. good bye json files go fk yourself.

--- routes/discovery.js
+++ routes/discovery.js
@@ -5,13 +5,22 @@ exports.view = function(req, res, next) {
 	var data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 	console.log(data)
 	console.log('in');
-	models.drive.find({}, function(err, dbdata){
-	
+
+
+	models.drive.find({}, function(err, dbdrivedata){
 		if (err){
 			throw err;
 		}
-		else{
-		res.render('discovery', { userprofile: data , dbprofile: dbdata });
-		}
+
+		models.user.find({}, function(err, dbuserdata){
+			if (err){
+				throw err;
+			}
+
+			res.render('discovery', { userprofile: data , dbdrive: dbdrivedata, dbuser: dbuserdata });
+		});
 	});
+
+
+
 };
