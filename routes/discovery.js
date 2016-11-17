7259716b63155d63d56949a7912aad60dc4756dc
Michael Zhong <m2zhong@ucsd.edu>
2016-11-17 03:22:42 -0800

    fixed logic error on adding rider and group id in discovery.js route file

--- routes/discovery.js
+++ routes/discovery.js
@@ -37,7 +37,7 @@ exports.create = function (req, res){
                 console.log("currently adding userId " + currRidersId[i]);
             }
             
-            if(currRidersId.indexOf(userId) != -1){
+            if(currRidersId.indexOf(userId) == -1){
                 currRidersId[currRidersId.length] = userId;
                 console.log('UserId to be uploaded ' + userId);
             }
@@ -56,17 +56,24 @@ exports.create = function (req, res){
     });
     
     models.user.find({'_id': userId}, function(err, userData){
+        // Check if userData is not empty, meaning user exists
         if (userData.length > 0){
+            
+            // Query and make copy of current user data from mongoose
             for(var i = 0; i < userData[0].groups.length; i++){
+                // Copy user's ride group ids
                 currGroupId[i] = userData[0].groups[i];
                 console.log("currently adding groupId  " + currGroupId[i]);
             }
-    
-            if(currGroupId.indexOf(groupId) != -1){
+            
+            // If user is not within the group
+            if(currGroupId.indexOf(groupId) == -1){
+                // add user to the group
                 currGroupId[currGroupId.length] = groupId;
                 console.log("GroupId to be uploaded " + currGroupId);
             }
             
+            // else user already in the goup. Do nothing
             else
                 console.log(groupId + " already exist");
         
