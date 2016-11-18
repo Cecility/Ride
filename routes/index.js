67fbafeddca142d3cfdb69db70131a8c49ad9b46
Hansol You <h5you@ucsd.edu>
2016-11-17 21:53:03 -0800

    still broken

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
+var models = require('../models');
+
 exports.view = function(req, res, next) {
 	res.render('index');
 }
 
 var sess;
-exports.create = function(req,res,next){
+exports.create = function(req, res, next){
 
 	sess = req.session;
 	sess.loggedin = "1";
@@ -11,35 +13,53 @@ exports.create = function(req,res,next){
 	sess.email = req.body.email;
 	sess.pic = req.body.pic;
 
-	var users = req.mongoose.users;
 
-	for(var i = 0; i < users.length; i++){
-		if(users[i].email == sess.email){
+	models.users.find({}, function(err, userData){
+		if (err){
+			throw err;
+		}
+
+
+
+		var userExist = "0";
+
+		for(var i = 0; i < users.length; i++){
+			if(userData[i].email == sess.email){
+				userExist = "1";
+			}
+		}
+
+		if(userExist == "0"){
 			res.sendStatus(200);
-			break;
 		}
-	}
 
-	var newUsers = new models.users({
-	    "username": sess.username,
-	    "email": sess.email,
-	    "pic": sess.pic,
-	    "groups": []
-	});
-
-
-	newUsers.save(afterSaving);
+		else{
+			var newUsers = new models.users({
+			    "username": sess.username,
+			    "email": sess.email,
+			    "pic": sess.pic,
+			    "groups": []
+			});
 
 
-
-	//res.sendStatus(200);
-	console.log("reached login route");
+			console.log("newUsers is " + newUsers);
 
 
-	function afterSaving(err){ // this is a callback
-		if(err) {
-			console.log(err); res.send(500);
+			newUsers.save(afterSaving);
 		}
-	}
 
+
+		//res.sendStatus(200);
+		console.log("reached login route");
+
+
+		function afterSaving(err){ // this is a callback
+			if(err) {
+				console.log(err); res.send(500);
+			}
+
+			res.redirect('/main');
+		}
+		
+	}
 }
